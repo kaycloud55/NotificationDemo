00e435f91439536b7859181c4e094a2a22512bd4
kaycloud <jiangyk55@gmail.com>
2020-01-04 14:44:25 +0800

    second

--- app/src/main/java/com/kaycloud/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/kaycloud/notificationdemo/MainActivity.kt
@@ -6,13 +6,17 @@ import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val CHANNEL_ID = "11111111"
     private val CHANNEL_ID_2 = "2222222"
+    private val CHANNEL_ID_3 = "22222"
+    private val CHANNEL_ID_4 = "2222342322"
 
 
     private val title1 = "title1"
@@ -28,53 +32,83 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createNotificationChannel()
+        val remoteViews = RemoteViews(packageName, R.layout.item_layout)
         var notification1 = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle(title1)
-            .setContentText(message1)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setAutoCancel(true)
+            .setShowWhen(true)
+            .setCategory(NotificationCompat.CATEGORY_SOCIAL)
+//            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setCustomContentView(remoteViews)
+//            .setContentTitle(title1)
+//            .setContentText(message1)
             .setGroup(GROUP_KEY_WORK_EMAIL)
-            .setStyle(NotificationCompat.InboxStyle())
-            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
             .build()
 
 
         var notification2 = NotificationCompat.Builder(this, CHANNEL_ID_2)
             .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle(title2)
-            .setContentText(message2)
-            .setStyle(NotificationCompat.InboxStyle())
+            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setCustomContentView(remoteViews)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setAutoCancel(true)
+            .setShowWhen(true)
+            .setCategory(NotificationCompat.CATEGORY_SOCIAL)
+//            .setContentTitle(title2)
+//            .setContentText(message2)
             .setGroup(GROUP_KEY_WORK_EMAIL)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
             .build()
 
-        var notificationSummary = NotificationCompat.Builder(this, CHANNEL_ID_2)
+        var notification3 = NotificationCompat.Builder(this, CHANNEL_ID_3)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setCustomContentView(remoteViews)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setAutoCancel(true)
+            .setShowWhen(true)
+            .setCategory(NotificationCompat.CATEGORY_SOCIAL)
+//            .setContentTitle(title2)
+//            .setContentText(message2)
+            .setGroup(GROUP_KEY_WORK_EMAIL)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .build()
+
+        var notification4 = NotificationCompat.Builder(this, CHANNEL_ID_4)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setCustomContentView(remoteViews)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setAutoCancel(true)
+            .setShowWhen(true)
+            .setCategory(NotificationCompat.CATEGORY_SOCIAL)
+//            .setContentTitle(title2)
+//            .setContentText(message2)
+            .setGroup(GROUP_KEY_WORK_EMAIL)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .build()
+
+        var notificationSummary = NotificationCompat.Builder(this, CHANNEL_ID_4)
             .setSmallIcon(R.drawable.ic_launcher_background)
             .setContentTitle(title2)
             .setContentText(message2)
-//            .setStyle(
-//                NotificationCompat.InboxStyle()
-//                    .addLine("$title2 $message2")
-//                    .addLine("$title1 $message1")
-//                    .addLine("$title1 $message1")
-//                    .addLine("$title1 $message1")
-//                    .addLine("$title1 $message1")
-//                    .addLine("$title1 $message1")
-//                    .addLine("$title1 $message1")
-//                    .addLine("$title1 $message1")
-//                    .addLine("$title1 $message1")
-//                    .setBigContentTitle("2 new messages")
-//                    .setSummaryText("user@example")
-//            )
-            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setShowWhen(true)
             .setGroup(GROUP_KEY_WORK_EMAIL)
-            .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_CHILDREN)
+//            .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_CHILDREN)
             .setGroupSummary(true)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
+            .build()
 
 //        for (id in 1..5) {
         with(NotificationManagerCompat.from(this)) {
             notify(notifyId++, notification1)
             notify(notifyId++, notification2)
+            notify(notifyId++, notification3)
+            notify(notifyId++, notification4)
             notify(notifyId++, notificationSummary)
         }
 //        }
@@ -94,11 +128,19 @@ class MainActivity : AppCompatActivity() {
             val channel2 = NotificationChannel(CHANNEL_ID_2, name, importance).apply {
                 description = descriptionText
             }
+            val channel3 = NotificationChannel(CHANNEL_ID_3, name, importance).apply {
+                description = descriptionText
+            }
+            val channel4 = NotificationChannel(CHANNEL_ID_4, name, importance).apply {
+                description = descriptionText
+            }
             // Register the channel with the system
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
             notificationManager.createNotificationChannel(channel2)
+            notificationManager.createNotificationChannel(channel3)
+            notificationManager.createNotificationChannel(channel4)
         }
     }
 
